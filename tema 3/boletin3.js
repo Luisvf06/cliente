e64890e873a76628f614e2c1d9de3acce8c280f0
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-17 22:48:14 +0200

    corrijo el push que hice por error y añado el boletin de BOM

---
+++ tema 3/boletin3.js
@@ -0,0 +1,40 @@
+/*En una nueva ventana, imprimir todas las propiedades del objeto navigator.*/
+let text = "<p>Browser CodeName: " + navigator.appCodeName + "</p>" +
+"<p>Browser Name: " + navigator.appName + "</p>" +
+"<p>Browser Version: " + navigator.appVersion + "</p>" +
+"<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>" +
+"<p>Browser Language: " + navigator.language + "</p>" +
+"<p>Browser Online: " + navigator.onLine + "</p>" +
+"<p>Platform: " + navigator.platform + "</p>" +
+"<p>User-agent header: " + navigator.userAgent + "</p>";
+
+document.write(text);
+/*Escribir un script que mostrará el nombre del navegador
+, la versión y el sistema operativo que esté utilizando. 
+Prueba tu programa en distintos navegadores, el objeto Navigator no está estandarizado.*/
+let text2 = "<p>Browser CodeName: " + navigator.appCodeName + "</p>" 
+document.write(text2);
+
+/*Crear dos enlaces, uno para abrir una nueva ventana y uno para cerrarla. 
+La nueva ventana mostrará este mensaje en una fuente grande : ​”El ojo es la ventana a tu alma “. 
+La nueva ventana se coloca en la esquina izquierda de la pantalla 
+, será redimensionable , tendrá una barra de desplazamiento, y estará en primer plano.*/
+
+
+/*En un cuadro de diálogo de alerta, muestre el alto y ancho en píxeles y la profundidad de color de su pantalla. Cada valor será separado por un salto de línea.
+Crear un programa que va a crear un reloj digital en el título. Utilice el método setInterval() para actualizar el título de la página una vez cada minuto con la hora actual.
+Crea una página web que realice las siguientes funciones desde la página principal. Crea botones para cada realizar cada una de las siguientes opciones:
+	- Abrir ventana flotante
+	- Abrir ventana flotante y detenerla
+	- Cerrar ventana flotante
+	- Dar foco a la ventana flotante
+	- Quitar foco ventana nueva
+	- Scroll absoluto (0, 1000)
+	- Scroll relativo (0, 10)
+Desde la ventana secundaria se podrá realizar:
+	- Mover absoluto (500, 150)
+	- Mover relativo (0,50)
+	- Redimensionar absoluto (500,500)
+	- Redimensionar relativo (-50, -50)
+Crear un programa en cuyo título del documento aparezca el siguiente texto animado circularmente (ver ​Ejemplo de texto animado circularmente​): ¡¡Sólo quedan 20 días de ofertas!!
+*/
