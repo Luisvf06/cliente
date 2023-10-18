1282caa8db471a1cb4fde7af2f6349dc20cf0ef4
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-18 19:09:14 +0200

    he hecho hasta el de abrir una ventana emergente con el ojo es la ventana... pero no entiendo nada, solo copio y pego

--- tema 3/boletin3.js
+++ tema 3/boletin3.js
@@ -1,5 +1,5 @@
 /*En una nueva ventana, imprimir todas las propiedades del objeto navigator.*/
-let text = "<p>Browser CodeName: " + navigator.appCodeName + "</p>" +
+/*let text = "<p>Browser CodeName: " + navigator.appCodeName + "</p>" +
 "<p>Browser Name: " + navigator.appName + "</p>" +
 "<p>Browser Version: " + navigator.appVersion + "</p>" +
 "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>" +
@@ -8,19 +8,24 @@ let text = "<p>Browser CodeName: " + navigator.appCodeName + "</p>" +
 "<p>Platform: " + navigator.platform + "</p>" +
 "<p>User-agent header: " + navigator.userAgent + "</p>";
 
-document.write(text);
+
+var ventana=window.open('', '_blank', 'width=400,height=200');
+ventana.document.write(text)
+*/
 /*Escribir un script que mostrará el nombre del navegador
 , la versión y el sistema operativo que esté utilizando. 
 Prueba tu programa en distintos navegadores, el objeto Navigator no está estandarizado.*/
-let text2 = "<p>Browser CodeName: " + navigator.appCodeName + "</p>" 
-document.write(text2);
+/*
 
+let text2 = "<p>Browser CodeName: " + navigator.appCodeName + "</p>" + '<p>Nombre navegador: ' + navigator.appName + '</p> Versión: ' + navigator.appVersion;
+document.write(text2);
+*/
 /*Crear dos enlaces, uno para abrir una nueva ventana y uno para cerrarla. 
 La nueva ventana mostrará este mensaje en una fuente grande : ​”El ojo es la ventana a tu alma “. 
 La nueva ventana se coloca en la esquina izquierda de la pantalla 
 , será redimensionable , tendrá una barra de desplazamiento, y estará en primer plano.*/
 
-
+		//Esta en el html
 /*En un cuadro de diálogo de alerta, muestre el alto y ancho en píxeles y la profundidad de color de su pantalla. Cada valor será separado por un salto de línea.
 Crear un programa que va a crear un reloj digital en el título. Utilice el método setInterval() para actualizar el título de la página una vez cada minuto con la hora actual.
 Crea una página web que realice las siguientes funciones desde la página principal. Crea botones para cada realizar cada una de las siguientes opciones:
