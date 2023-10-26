319503794591a4299435027384ad14041f3da9bf
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-26 16:13:24 +0200

    me faltan por hacer los del botones dentro ed la nueva ventana y el último

--- tema 3/E6BOW.js
+++ tema 3/E6BOW.js
@@ -1,17 +1,18 @@
 window.addEventListener('load',inicializar);
     var ventana;
     function inicializar(){
-        document.getElementById('abrir').addEventListener('click','abrirVentana');
-        document.getElementById('abrir-detener').addEventListener('click','abrirVentanaDetenida');
-        document.getElementById('cerrar').addEventListener('click','cerrarVentana');
-        document.getElementById('dar-foco').addEventListener('click','darFocoVentana');
-        document.getElementById('quitar-foco').addEventListener('click','quitarFocoVentana');
+        document.getElementById('abrir').addEventListener('click',abrirVentana);
+        document.getElementById('abrir-detener').addEventListener('click',abrirVentanaDetenida);
+        document.getElementById('cerrar').addEventListener('click',cerrarVentana);
+        document.getElementById('dar-foco').addEventListener('click',darFocoVentana);
+        document.getElementById('quitar-foco').addEventListener('click',quitarFocoVentana);
+        document.getElementById('scroll-absoluto').addEventListener('click',scrollAbsoluto)
+        document.getElementById('scroll-relativo').addEventListener('click',scrollRelativo);
     }
 
 function abrirVentana() {
 ventana = window.open("", "_blank", "width=300px","height=300%","resizable=yes","scrollbars=yes");
-ventana.document.write('<div style="height:2000px; border: 2px solid black;"><input type="button" id="scroll" value="scroll absoluto"></input></div>');
-
+ventana.document.write('<head><script type="text/javascript" src="E6BOW.js"></script></head><div style="height:2000px; border: 2px solid black;"><input type="button" id="scroll" value="scroll absoluto"></input><br><input type="button" id="scrollRel" value="scroll relativo"></input><br><input type="button" id="redimensionarAbs" value="Redimensionar absoluto"></input><br><input type="button" id="redimensionarRel" value="Redimensionar relativo"></input></div>');
 }
 
 function abrirVentanaDetenida() {
@@ -27,34 +28,36 @@ ventana.close();
 }
 }
 
-function darFocoAVentana() {//
+function darFocoVentana() {//
 // No sé si es esto lo que pide
 if (ventana) {
 ventana.focus();
 }
 }
 
-function quitarFocoAVentana() {
+function quitarFocoVentana() {
 // Quitamos el foco a la ventana
 if (ventana) {
   ventana.blur();
 }
 }
-
 function scrollAbsoluto() {
-if (ventana) {
-ventana.scrollTo(0, 1000);
-}
-}
+  window.scrollTo(0, 1000);
+  }
+
+
 
 function scrollRelativo() {
-if (ventana) {
-ventana.scrollBy(0, 10);
+window.scrollBy(0, 100);
 }
+ventana.addEventListener('load',inicializar2);
+function inicializar2(){
+  ventana.getElementById('scroll').addEventListener('click',scrollAbsoluto2);
+  ventana.getElementById('scrollRel').addEventListener('click',scrollRelativo);
 }
-
-
-
+function scrollAbsoluto2() {//ventana emergente
+ventana.scrollTo(0, 1000);
+}
 // Inicializamos la ventana
 window.addEventListener("load", asignarEventos);
     
