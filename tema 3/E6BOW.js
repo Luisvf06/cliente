7a7c5423a4e0d709afdcf6808295bcaafb73a36a
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-26 12:55:07 +0200

    no me funcionan los botones de scroll y no sé hacer los siguientes

--- tema 3/E6BOW.js
+++ tema 3/E6BOW.js
@@ -2,29 +2,33 @@ window.addEventListener('load',inicializar);
     var ventana;
     function inicializar(){
         document.getElementById('abrir').addEventListener('click','abrirVentana');
-        document.getElementById('abrir-detener').addEventListener('clic','abrirVentanaDetenida');
+        document.getElementById('abrir-detener').addEventListener('click','abrirVentanaDetenida');
+        document.getElementById('cerrar').addEventListener('click','cerrarVentana');
+        document.getElementById('dar-foco').addEventListener('click','darFocoVentana');
+        document.getElementById('quitar-foco').addEventListener('click','quitarFocoVentana');
     }
+
 function abrirVentana() {
-// Creamos una nueva ventana
-ventana = window.open("", "_blank", "width=500,height=500,resizable=yes,scrollbars=yes");
+ventana = window.open("", "_blank", "width=300px","height=300%","resizable=yes","scrollbars=yes");
+ventana.document.write('<div style="height:2000px; border: 2px solid black;"><input type="button" id="scroll" value="scroll absoluto"></input></div>');
 
 }
 
 function abrirVentanaDetenida() {
-// Creamos una nueva ventana
+// La ventana se cierra a los 5 segundos
 ventana = window.open("", "ventana-flotante", "width=500,height=500,resizable=yes,scrollbars=yes,location=no");
 ventana.setTimeout(cerrarVentana,5000)
 }
 
 function cerrarVentana() {
-// Cerramos la ventana
+
 if (ventana) {
 ventana.close();
 }
 }
 
-function darFocoAVentana() {//voy por este
-// Damos foco a la ventana
+function darFocoAVentana() {//
+// No sé si es esto lo que pide
 if (ventana) {
 ventana.focus();
 }
@@ -33,34 +37,23 @@ ventana.focus();
 function quitarFocoAVentana() {
 // Quitamos el foco a la ventana
 if (ventana) {
-ventana.blur();
+  ventana.blur();
 }
 }
 
 function scrollAbsoluto() {
-  // Establecemos el scroll absoluto
 if (ventana) {
 ventana.scrollTo(0, 1000);
 }
 }
 
 function scrollRelativo() {
-// Establecemos el scroll relativo
 if (ventana) {
 ventana.scrollBy(0, 10);
 }
 }
 
-// Asociamos los eventos a los inputs type="button"
-function asignarEventos() {
-document.querySelector("#abrir").addEventListener("click", abrirVentana);
-document.querySelector("#abrir-detener").addEventListener("click", abrirVentanaDetenida);
-document.querySelector("#cerrar").addEventListener("click", cerrarVentana);
-document.querySelector("#dar-foco").addEventListener("click", darFocoAVentana);
-document.querySelector("#quitar-foco").addEventListener("click", quitarFocoAVentana);
-document.querySelector("#scroll-absoluto").addEventListener("click", scrollAbsoluto);
-document.querySelector("#scroll-relativo").addEventListener("click", scrollRelativo);
-}
+
 
 // Inicializamos la ventana
 window.addEventListener("load", asignarEventos);
