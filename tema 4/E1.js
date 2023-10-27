ae143ce73b0775b9fec58c2e80e1581024b3a443
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-27 14:41:09 +0200

    empezado a hacer el 1

---
+++ tema 4/E1.js
@@ -0,0 +1,12 @@
+function esPerfecto(numero){
+    var divisores;
+    var suma;
+    for(let i=0;i<=numero;i++){
+        if (numero%i==0){
+            divisores.push(i);
+        }
+    for (let i=0;i<divisores.length<i++){
+
+    }
+    }
+}
