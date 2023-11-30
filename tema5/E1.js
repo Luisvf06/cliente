20b5901652697b4f77011652a2a569052273a95e
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-11-30 22:44:07 +0100

    el campo edad o devuelve NaN o nada, no consigo calcular la edad

--- tema5/E1.js
+++ tema5/E1.js
@@ -1,16 +1,50 @@
 window.addEventListener('load',inicializar);
 
 function inicializar(){
-    document.getElementById('boton').addEventListener('click',recogerDatos())
+    
+    function mostrarDatos() {
 
-
-    function recogerDatos(){
-            var formulario= document.getElementById("formulario")//aqui tengo el objeto formulario, que es una array de sus elementos
-            return formulario
-    }
-    function convertirMayusc(datos){
-        for (let dato=0; dato<datos.length;dato++){
-            
+        var formulario = document.getElementById('formulario');//variable que almacena el objeto formulario
+    
+        // Inicializar la variable de mensaje
+        var mensaje = '';
+    
+        // Itero sobre los elementos del formulario
+        for (var i = 0; i < formulario.elements.length; i++) {
+            var elemento = formulario.elements[i];
+            // Verificar si el elemento es un campo de entrada y no es de tipo "button"
+            if (elemento.tagName === 'INPUT' && elemento.type !== 'button') {
+                // convierto a mayuscula
+                mensaje += elemento.name + ': ' + elemento.value.toUpperCase() + '\n';
+            } else if (elemento.tagName === 'SELECT') {
+                // agrega a la variable el valor de los elementos tipo select
+                mensaje += elemento.name + ': ' + elemento.options[elemento.selectedIndex].text.toUpperCase() + '\n';
+            }
         }
+    
+        // nueva ventana
+        var nuevaVentana = window.open('', '_blank');
+        nuevaVentana.document.write('<pre>' + mensaje + '</pre>');
     }
+    
+    // busca el boton
+    var boton = document.getElementById('boton');
+    
+    // funcion del boton
+    boton.addEventListener('click', mostrarDatos);
+
+    //ejercicio3 añadir edad despues de fecnac y que no se pueda editar
+    function calcularEdad(){
+        var hoy= new Date();
+        var diaC= document.getElementById('dia').value;
+        var mesC=document.getElementById('mes').value;
+        var anhoC=document.getElementById('año').value;
+        
+        var cumple= new Date(anhoC,mesC-1,diaC);
+        var edadMs=hoy-cumple;
+        var edad=Math.floor(edadMs/365*24*60*60*1000)
+        document.getElementById('edad').value = edad;
+    }
+    var boton2=document.getElementById('boton2');
+    boton2.addEventListener('click',calcularEdad)
 }
