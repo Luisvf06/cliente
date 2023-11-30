bf89c3427401a8cdbd662c1b0dfc984b794fc07c
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-11-30 14:42:51 +0100

    princpio del ejercicio 2

--- tema5/E1.js
+++ tema5/E1.js
@@ -2,11 +2,7 @@ window.addEventListener('load',inicializar);
 
 function inicializar(){
     document.getElementById('boton').addEventListener('click',recogerDatos())
-    for(let i=0;formulario.elements.length;i++){
-        if(formulario.elements[i].type=="text"){
-            formulario.elements[i]=formulario.elements[i].toUpperCase()
-        }
-    }
+
 
     function recogerDatos(){
             var formulario= document.getElementById("formulario")//aqui tengo el objeto formulario, que es una array de sus elementos
