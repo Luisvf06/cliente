a2e47364bc6ead699d0d70037fc585c984690fd2
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-12-14 14:16:06 +0100

    pruebas hechas para mercedes

--- examen/ejercicio1/prueba.js
+++ examen/ejercicio1/prueba.js
@@ -1,6 +1,12 @@
-ci1= new Cita(3,'01/01/2024')
-ci2=new Cita(1,'31/12/2023')
-cl =new Cliente('pepe','apellidos','20258147g')
-cl.crearCita(ci1)
+let ci1= new Cita(3,'01/01/2024')
+let ci2=new Cita(1,'31/12/2023')
+let ci3=new Cita(3,'01/01/2023')
+let cl =new Cliente('pepe','apellidos','20258147g')
 
-document.write(cl.ci1)
+cl.crearCita(ci1)
+cl.crearCita(ci2)
+cl.crearCita(ci3)
+
+console.log(cl.nombre)
+console.log(cl.citasPendientes)
+document.write(cl.citasPendientes[1].sala)
--- examen/ejercicio2/prueba.js
+++ examen/ejercicio2/prueba.js
@@ -1,6 +1,7 @@
 var i1=new integrante('12345W','Pepe','perez')
 var i2=new integrante('23456Q','Paco','Sanz')
 var i3=new integrante('34567E','Lolo','man')
+var i4=new integrante('34567P','Luis','Apellidodeluis')
 var b1= new Banda('Chunguitos','2000',123456789,'Soul')
 var b2=new Banda('Camela','1999',78945612,'pop')
 var b3=new Banda('Fito','2015',45678912,'pop')
@@ -13,4 +14,4 @@ b1.integrantes.push(i2)
 document.write('<br>',b1.mostrarBanda(),'<br>')
 
 b1.integrantes.push(i3);
-document.write('<br>',b1.mostrarBanda(),'<br>')
+document.write('<br>',b1.mostrarBanda(),'<br>') 
