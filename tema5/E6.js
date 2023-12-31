c6db95b15118c3c3e7c76014411d43d70163cd4a
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-12-31 23:19:32 +0100

    ejercicoi 6 medio hecho, no sé hacer el envío al servidor

---
+++ tema5/E6.js
@@ -0,0 +1,35 @@
+/*Crear un formulario que utiliza un campo de entrada de texto, que pida
+al usuario que escriba su nombre en letras mayúsculas. El botón de
+envío será una imagen. Validar que el formulario no está vacío y que
+el usuario ha escrito su nombre sólo con letras mayúsculas. Envíe el
+formulario a un programa de servidor si es correcto.*/
+window.addEventListener('load',inicializar);
+
+function inicializar(){
+    var formulario = document.getElementById('formulario');
+    var nombreInput = document.getElementById('nombre');
+    var mensajeError = document.getElementById('mensaje');
+
+    function validar() {
+        mensajeError.style.display = 'none';
+        mensajeError.innerHTML = '';
+
+        if (nombreInput.value === "") {
+            mensajeError.style.display = 'block';
+            mensajeError.appendChild(document.createTextNode('El campo no puede estar en blanco'));
+        } else if (nombreInput.value !== nombreInput.value.toUpperCase()) {
+            mensajeError.style.display = 'block';
+            mensajeError.appendChild(document.createTextNode('El nombre debe estar en mayúsculas'));
+        } else {
+
+            document.getElementById('boton').addEventListener('click', enviar);
+        }
+    }
+    function enviar(){
+        pass
+        //no sé hacer esta parte
+    }
+    var boton=document.getElementById('boton')
+    boton.addEventListener('click',validar)
+    
+}
