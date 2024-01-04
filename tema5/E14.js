4ddaf7ff88f24ee0a60009a2f9254ae46220442f
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2024-01-04 23:22:05 +0100

    consigo que se creen las cookies pero no que salte el mensaje de la oferta

--- tema5/E14.js
+++ tema5/E14.js
@@ -1,35 +1,54 @@
 /*Crear un formulario que contiene un conjunto de casillas de verificación con diferentes tipos de cafés espresso, cappuccino, moka, y así sucesivamente. Pida al usuario su nombre y número de habitación y seleccionar un tipo de café. Indicar al usuario que va a enviar el café a su número de habitación. Cree las cookie para recordar las preferencias del usuario. La próxima vez que el usuario entre en la página, en introduzca su nombre de usuario, se dirá que hay una tarifa especial para su (utilice el valor de la cookie) café favorito.*/
 window.addEventListener('load', inicializar);
-
+window.addEventListener('load',preferencias)
 function inicializar() {
-    var formulario = document.getElementById('formulario');
-    var boton = document.getElementById('boton');
-
-    boton.addEventListener('click', function() {
-        var usuario = document.getElementById('nombre').value;
-        var habitacion = document.getElementById('habitacion').value;
-        var cafe = document.getElementById('cafe').value;
-
-        // Validar que los campos obligatorios estén completos
-        if (usuario && habitacion && cafe) {
-            crearCookie(usuario, habitacion, cafe);
-        } else {
-            alert("Por favor, complete todos los campos antes de enviar.");
+    function crearCookie(nombre,valor,dias){
+        var caduca="";
+        if(dias){
+            var fecha=new Date()
+            fecha.setTime(fecha.getTime()+(dias*24*60*60*1000));
+            caduca=";caduca= " + fecha.toUTCString();
         }
-    });
-    function crearCookie(usuario, habitacion, cafe) {
-        // Obtén la fecha actual y agrega 1 día
-        var expirationDate = new Date();
-        expirationDate.setDate(expirationDate.getDate() + 1);
-
-        // Formatea la fecha de expiración en el formato de cookie
-        var expires = "expires=" + expirationDate.toUTCString();
-
-        // Crea la cookie
-        document.cookie = `usuario=${usuario}; habitacion=${habitacion}; cafe=${cafe}; ${expires}; path=/;`;
-
-        alert("Cookie creada. La próxima vez que ingreses tu nombre, se mostrará tu café favorito.");
+        document.cookie= nombre + "="+ valor+ caduca+ "; path=/";
     }
+
+    function usarCookie(nombre){
+        var nombre= nombre + "=";
+        var lista = document.cookie.split(";");
+        for (var i=0; i<lista.length;i++){
+            var elemento = lista[i];
+            while (elemento.charAt(0)==' '){
+                elemento = elemento.substring(1,elemento.length);
+            }
+            if (elemento.indexOf(nombre)==0){
+                return elemento.substring(nombre.length, elemento.length);
+            }
+        }
+        return null;
+    }
+    function datos(){
+        var usuario= document.getElementById('nombre').value;
+        var habitacion= document.getElementById("habitacion").value;
+        var cafe= document.getElementById('cafe').value;
+
+        if (usuario && habitacion && cafe){
+            crearCookie('usuario',usuario,365);
+            crearCookie('habitacion',habitacion,365);
+            crearCookie('cafe',cafe,365);
+            alert('Café enviado a la habitacion '+ habitacion);
+        }else{ alert('Completa el formulario')
+        }
+    }
+    function preferencias(){
+        var usuario= usarCookie('usuario');
+        var cafe = usarCookie('cafe');
+        if (usuario &&cafe){
+            alert(usuario+ 'hay una oferta especial de '+ cafe)
+        }
+    }
+    var boton= document.getElementById('boton');
+    boton.addEventListener('click',datos)
+
 }
 
 
