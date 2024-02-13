17ceee7016d64d2cc58032946e7879d0c213f829
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2024-02-13 10:50:00 +0100

    nuevos permisos

--- angular17/src/app/watchlist/watchlist.component.ts
+++ angular17/src/app/watchlist/watchlist.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { PeliculasService } from '../services/peliculas.service';}
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.scss'
 })
-export class WatchlistComponent {
+export class WatchlistComponent implements OnInit{
+  peliculas: any[] = [];
 
+  constructor(private peliculasService: PeliculasService) {}
+
+  ngOnInit(): void {
+    this.getPeliculas();
+  }
+  getPeliculas(): void {
+    this.peliculasService.getPeliculas().subscribe(
+      (data: any) => {
+        // Accede a la propiedad 'results' del objeto de respuesta
+        this.peliculas = data.results;
+      },
+      (error) => {
+        console.error('Error al obtener las películas', error);
+      }
+    );
+  }
 }
