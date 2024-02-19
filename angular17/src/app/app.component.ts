f4ab67e7eb62e0009b438766959f3f26a76c24c9
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2024-02-19 14:41:46 +0100

    segur con e buscador

--- angular17/src/app/app.component.ts
+++ angular17/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { SearchService } from './search.service'; // Importa el servicio
+import { SearchService } from './services/search.service'; // Importa el servicio
 
 @Component({
   selector: 'app-root',
