edf3c396261310c14ce2c5a5488a5052a8bfb166
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2024-03-04 14:43:19 +0100

    wathclist medio ehcha falta que muestre cosas

--- angular17/src/app/services/watchlist.service.ts
+++ angular17/src/app/services/watchlist.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WatchlistService {
+  private account_id = '20930787';
+  private apiKey = '4431fed8390b02d6c28655feb536156a'; // Agrega tu clave de API aquí
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWatchlistMovies(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.apiKey}`
+    });
+
+    return this.http.get<any>(`https://api.themoviedb.org/3/account/${this.account_id}/watchlist/movies?language=en-US&page=1&sort_by=created_at.asc`, { headers });
+  }
+
+  removeMovieFromWatchlist(movieId: number): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.apiKey}`
+    });
+
+    const body = {
+      media_type: 'movie',
+      media_id: movieId,
+      watchlist: false
+    };
+
+    return this.http.post<any>(`https://api.themoviedb.org/3/account/${this.account_id}/watchlist`, body, { headers });
+  }
 }
--- angular17/src/app/watchlist/watchlist.component.ts
+++ angular17/src/app/watchlist/watchlist.component.ts
@@ -1,4 +1,3 @@
-// watchlist.component.ts
 import { Component, OnInit } from '@angular/core';
 import { WatchlistService } from '../services/watchlist.service';
 
@@ -21,10 +20,32 @@ export class WatchlistComponent implements OnInit {
       .subscribe((data: any) => {
         this.movies = data.results;
         this.movies.sort((a, b) => b.vote_average - a.vote_average);
-      }, (error: any) => { // Especifica el tipo de error como any o el tipo adecuado si lo conoces
+      }, error => {
         console.error('Error fetching watchlist movies', error);
       });
   }
-  
-  
+
+  borrarPelicula(movieId: number): void {
+    this.watchlistService.removeMovieFromWatchlist(movieId)
+      .subscribe(response => {
+        console.log('Movie removed successfully');
+        this.getWatchlistMovies();
+      }, error => {
+        console.error('Error removing movie from watchlist', error);
+      });
+  }
+
+  getMovieImageUrl(posterPath: string): string {
+    if (posterPath) {
+      return `https://image.tmdb.org/t/p/w500/${posterPath}`;
+    } else {
+      return 'https://via.placeholder.com/150';
+    }
+  }
+
+  // Define la función elegirPeli para manejar la selección de películas
+  elegirPeli(movie: any): void {
+    console.log('Película elegida:', movie);
+    // Aquí puedes implementar la lógica que necesites cuando se elige una película
+  }
 }
